1d84cb0e347946a6f2abac2e6bd75e6a20738930
fajjarnr <zfajart@gmail.com>
2022-01-07 12:33:05 +0700

    mongo atlas

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 // express session
-const session = require("express-session");
-const flash = require("connect-flash");
+const session = require('express-session');
+const flash = require('connect-flash');
 // cors
-const cors = require("cors");
+const cors = require('cors');
 // method override
-const methodOverride = require("method-override");
+const methodOverride = require('method-override');
 // mongoose
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const mongoAtlas =
-  "mongodb+srv://fajjarnr:staycation@cluster0.00xsy.mongodb.net/db_staycation?retryWrites=true&w=majority";
+const mongoAtlas = process.env.MONGODB;
 
-mongoose.connect(mongoAtlas, {
+mongoose.connect(process.env.MONGODB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 // router admin
-var adminRouter = require("./routes/admin");
-var apiRouter = require("./routes/api");
+var adminRouter = require('./routes/admin');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 app.use(
   session({
-    secret: "keyboard cat",
+    secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
     cookie: { maxAge: 60000 },
@@ -47,22 +46,22 @@ app.use(
 );
 app.use(flash());
 app.use(cors());
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  "/sb-admin",
-  express.static(path.join(__dirname, "node_modules/startbootstrap-sb-admin-2"))
+  '/sb-admin',
+  express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2'))
 );
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // admin
-app.use("/admin", adminRouter);
-app.use("/api/v1/member", apiRouter);
+app.use('/admin', adminRouter);
+app.use('/api/v1/member', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -73,11 +72,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
